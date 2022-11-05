41521bf61a11d34a69c5e4afcbe7a1c9b88df39e
3Li <91336488+EliStillCantCode@users.noreply.github.com>
2022-11-05 12:59:50 +0000

    Update rockpaperscissors.py
    - removed case-sensitivity
    - cleaner

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,48 +1,42 @@
-goAgain = True  # GAME LOOP
+import random
+import time
+goAgain = True
 while goAgain:
-    import time
-
-    rpsOptions = ["Rock", "Paper", "Scissors"]  # VARIABLES
-    userChoice = input('Rock, Paper, or Scissors? ')
-
-    import random  # RANDOM PICKER
-    rpsIndexChoice = random.randint(0, 2)
-    rpsChoice = rpsOptions[rpsIndexChoice]
-
-    print('Thinking...')  # DIALOGUE
-    time.sleep(3)
-
-    if userChoice.lower() == "rock":  # OUTCOMES
-        if rpsChoice == "Rock":
-            print('It is a draw, I chose Rock!')
-        if rpsChoice == "Paper":
-            print('I win, Paper covers Rock!')
-        if rpsChoice == "Scissors":
-            print('You win, Rock blunts Scissors!')
-
-    elif userChoice.lower() == "paper":
-        if rpsChoice == "Rock":
-            print('You win, Paper covers Rock!')
-        if rpsChoice == "Paper":
-            print('It is a draw, I chose Paper!')
-        if rpsChoice == "Scissors":
-            print('I win, Scissors cuts Paper!')
-
-    elif userChoice.lower() == "scissors":
-        if rpsChoice == "Rock":
-            print('I win, Rock blunts Scissors!')
-        if rpsChoice == "Paper":
-            print('You win, Scissors cuts Paper!')
-        if rpsChoice == "Scissors":
-            print('It is a draw, I chose Scissors!')
-
+    userRPS = input("Rock, Paper, or Scissors? ")
+    #
+    if userRPS.lower() not in ['rock', 'paper', 'scissors']:
+        print("\n\tOnly input ROCK, PAPER, or SCISSORS")
     else:
-        print('Please choose from Rock, Paper or Scissors.')
-
-    another = input('\nWould you like to go again? [y/n] ')  # PLAY AGAIN?
-    if another.lower() in ['y', 'yes']:
+        #
+        print("\n\tPicking...")
+        time.sleep(3)
+        print("\n\tFiguring out who won...\n")
+        #
+        time.sleep(3)
+        options = ['rock', 'paper', 'scissors']
+        computerRandom = random.randrange(0,2)
+        computerRPS = options[computerRandom]
+        #
+        if userRPS.lower() == "rock":
+            if computerRPS == "rock": print("We drew, I chose rock.")
+            elif computerRPS == "paper": print("I win, I chose paper.")
+            else: print("You win, I chose scissors.")
+        #
+        elif userRPS.lower() == "paper":
+            if computerRPS == "rock": print("You win, I chose rock.")
+            elif computerRPS == "paper": print("We drew, I chose paper.")
+            else: print("I win, I chose scissors.")
+        #
+        elif userRPS.lower() == "scissors":
+            if computerRPS == "rock": print("I win, I chose rock.")
+            elif computerRPS == "paper": print("You win, I chose paper.")
+            else: print("We drew, I chose scissors.")
+    #
+    time.sleep(3)
+    anotherGame = input("\nDo you want to go again?[Y/N] ")
+    if anotherGame.lower() not in ['y', 'yes']:
+        print("\n\tOk, see you soon!")
+        goAgain = False
+    else:
         print('\n')
         goAgain = True
-    else:
-        goAgain = False
-        exit()
